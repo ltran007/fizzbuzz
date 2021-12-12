0aa04f0e0ed37feddd3d803d19228d3f52eef8aa
ltran7 <ltran@isabelgroup.eu>
2021-12-12 13:16:22 +0100

    Implement FizzBuzz (Part 1)

---
+++ src/main/java/com/ltran/fizzbuzz/FizzBuzz.java
@@ -0,0 +1,28 @@
+package com.ltran.fizzbuzz;
+
+import java.util.stream.IntStream;
+
+public class FizzBuzz {
+
+    public static void main(String[] args) {
+        fizzbuzz();
+    }
+
+    public static void fizzbuzz() {
+        IntStream.rangeClosed(1, 100)
+                .mapToObj(FizzBuzz::calculateFizzBuzz)
+                .forEach(System.out::println);
+    }
+
+    public static String calculateFizzBuzz(int number) {
+        if (((number % 3) == 0) && ((number % 5) == 0))
+            return "FizzBuzz";
+        else if ((number % 3) == 0)
+            return "Fizz";
+        else if ((number % 5) == 0)
+            return "Buzz";
+        else
+            return String.valueOf(number);
+    }
+
+}
